075dc26f706337d3ed48c4a99ed8055b37799713
ismael perez <ismaelperezemail@gmail.com>
2017-08-13 20:36:47 -0700

    added html for create user view

--- routes/patients.js
+++ routes/patients.js
@@ -7,7 +7,7 @@ var knex = require('knex')({
   }
 });
 
-//GET ALL PLAYERS
+//GET ALL PATIENTS
 router.get('/', function(req, res, next) {
   knex.raw(`select * from patients`)
     .then(function(data) {
@@ -18,39 +18,38 @@ router.get('/', function(req, res, next) {
     })
 });
 
-// //FORM FOR CREATING NEW PLAYER******
-// router.get('/new', function(req, res, next) {
-//   //make a join table to join doctor with patient on line 38
-//   knex.raw('select * from patients')
-//   //*****still have to make new patient view*****
-//   res.render('createPatient');
-// });
-//
-//
-// //SHOW SINGLE PLAYER INFO*******
-// router.get('/:id', function(req, res, next) {
-//   //make a join table to join doctor with patient on line 24
-//   knex.raw(`select * from patients`)
-//     .then(function(data) {
-//       res.render('list', {
-//         title: 'Patients',
-//         list: data.rows[0]
-//       });
-//       //console.log('working');
-//     })
-// });
-//
-// //CREATE NEW PLAYER******
-// router.post('/', function(req, res, next) {
-//   console.log(req.body);
-//   knex.raw(`insert into patients() values ('$req.body.whatever', `)
-//     .then(function(data) {
-//       //have to create editplayer view
-//       res.render('/editPlater')
-//     });
-// });
-//
-// //GET EDIT FORM FOR UPDATING PLAYER******
+//FORM FOR CREATING NEW PATIENTS******
+router.get('/new', function(req, res, next) {
+  knex.raw('select * from patients')
+  res.render('createPatient');
+});
+
+
+//SHOW SINGLE PATIENT INFO*******
+router.get('/:id', function(req, res, next) {
+  //make a join table to join doctor with patient on line 24
+  knex.raw(`select patients.*, doctors.name as doctors_name from patients, doctors join appt on doctors.id = doctor_id and doctor_id = patient_id where patients.id = ${req.params.id} `)
+    .then(function(data) {
+      console.log(data.rows);
+      res.render('patientInfo', {
+        title: 'Patients',
+        list: data.rows
+      });
+    })
+});
+
+//CREATE NEW PATIENT******
+router.post('/', function(req, res, next) {
+  knex.raw(`insert into patients values(${req.body.res})`)
+  .then(function(data) {
+    res.render('list', {
+      title: 'Patients',
+      list: data.rows
+    });
+  })
+});
+
+// //GET EDIT FORM FOR UPDATING PATIENT******
 // router.post('/:id/edit', function(req, res, next) {
 //   knex.raw(`select * from patients join something`)
 //     .then(function(data) {
@@ -58,7 +57,7 @@ router.get('/', function(req, res, next) {
 //     });
 // });
 //
-// //UPDATE PLAYER******
+// //UPDATE PATIENT******
 // router.post('/:id', function(req, res, next) {
 //   knex.raw(`Update patients set name = ${req.body.name}`)
 //     .then(function(data) {
@@ -66,7 +65,7 @@ router.get('/', function(req, res, next) {
 //     });
 // });
 //
-// //DELETE PLAYER******
+// //DELETE PATIENT******
 // router.post('/:id/delete', function(req, res, next) {
 //   knex.raw(`delete from patients where ...`)
 //     .then(function(data) {
